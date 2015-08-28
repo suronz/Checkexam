42afe0279ef93815ea7b0c7695debe99ef7b4a18
Tamal <Tamal@192.168.0.2>
2015-08-28 12:43:49 +0530

    DBHelper(becasue of db name constant)

--- exam/src/com/util/DBHelper.java
+++ exam/src/com/util/DBHelper.java
@@ -7,15 +7,18 @@ public class DBHelper {
 	
 	private final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
     //private final static String DB_URL = "jdbc:mysql://localhost:3306/epariksh_exam_db";
-	private final static String DB_URL = "jdbc:mysql://localhost:3306/epariksh_exam_db";
+	private final static String DB_URL = "jdbc:mysql://localhost:3306/epariksh_angelinst_exam_db";
+	public final static String DB_NAME = "epariksh_angelinst_exam_db.";
+	public final static String ANGEL_INST_ADMIN1 = "angel_admin1";
+	public final static String ANGEL_INST_ADMIN2 = "angel_admin2";
     
 	public static Connection getConnection() {
-    	  Connection conn = null;
-		  String userName = "epariksh_test"; 
-		  String password = "test@123";
-			//Connection conn = null;
-		  //String userName = "root"; 
-		 // String password = "root";
+    	 /*Connection conn = null;
+		  String userName = "epariksh_exam"; 
+		  String password = "ParikshaPanel$125";*/
+		Connection conn = null;
+		  String userName = "root"; 
+		  String password = "";
 		  try {
 		  Class.forName(JDBC_DRIVER);
 		  conn = DriverManager.getConnection(DB_URL,userName,password);
